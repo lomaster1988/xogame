4e26bdce7ee1be4896b9de3b5062b5a74ded049d
Sergei Yermolin <y.serge@gmail.com>
2023-09-13 10:11:23 +0300

    changes v1.0

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-let elements = document.querySelector('form');
 
-for(i=0; i<elements.length; i++){
-    let price = Number(document.querySelector('.price').value);
-    let firstPayment = Number(document.querySelector('.first-payment').value);
-    let period1 = Number(document.querySelector('.period').value);
-    let sum1 = price - firstPayment;
-    let rate1 = Number(document.querySelector('.rate').value);
- elements[i].addEventListener("input", getPayment(sum1,period1,rate1))
+
+
+function drawMove(event){
+ console.log(event.target.className);
+ 
+ let square = document.querySelector('.'+event.target.className);
+ 
+ let currentPlayer = document.querySelector('.player').innerHTML;
+
+ if(square.innerHTML===''){
+//проверяем, что клеточка пустая: если это так, то ставим X или O,
+//если там уже что-то нарисовано, то ничего не делаем
+
+if(currentPlayer === 'Player1'){//проверяем, чей сейчас ход
+ square.innerHTML = 'X';
+ document.querySelector('.player').innerHTML = 'Player2';
 
   }
+  else {
+    square.innerHTML = 'O';
+    document.querySelector('.player').innerHTML = 'Player1';
+
+  }
+  
+}
 
 
- 
+
+
+
+
+}
+
+let gameField = document.querySelector('.gameField');
+
+gameField.addEventListener('click', drawMove);
